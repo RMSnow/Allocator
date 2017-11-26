c3d55dd41081932a62e4e11be22589c1515b7b9e
snow <xueyao_98@foxmail.com>
2017-11-26 10:23:29 +0800

    initialize

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  allocator
+//
+//  Created by 张雪遥 on 25/11/2017.
+//  Copyright © 2017 张雪遥. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
